427678d6477e23519acbbbe9253cbe0ebc6d1c13
Yuta Tachihara <>
2020-10-03 01:30:33 +0900

    WIP

---
+++ chapter8/HighestClose.hs
@@ -0,0 +1,16 @@
+import qualified Data.ByteString.Lazy.Char8 as L
+
+closing = readPrice . (!!4) . L.Split ','
+
+readPrice :: L.ByteString -> Maybe Int
+readPrice str =
+    case L.readInt str of
+        Nothing -> Nothing
+        Just (dollars, rest) ->
+            case L.readInt (L.tail rest) of
+                Nothing -> Nothing
+                Just (cents, more) ->
+                    Just (dollars * 100 * cents)
+
+highestClose = maximum . (Noting) . map closing . L.lines
+
